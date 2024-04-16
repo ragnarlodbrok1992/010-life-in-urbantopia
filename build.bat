0e8ba9b0aad7f830520124813b62487702e5cc03
ragnarlodbrok1992 <maraus92@gmail.com>
2024-04-16 18:33:52 +0200

    WIP

--- build.bat
+++ build.bat
@@ -1,5 +1,8 @@
 @echo off
 SET compiler_dir="C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\14.31.31103\bin\Hostx64\x64\cl.exe"
+SET sdl_include_dir="D:\libraries\SDL2-2.28.2\include"
+SET sdl_link_dir="D:\libraries\SDL2-2.28.2\lib\x64"
+SET sdl_dll_dir="D:\libraries\SDL2-2.28.2\lib\x64"
 
 REM Create build directory if not exists
 IF NOT EXIST build mkdir build
@@ -8,10 +11,15 @@ pushd build
 
 REM Compile
 
+REM Copy SDL2.dll to build directory
+copy %sdl_dll_dir%\SDL2.dll .
+
 %compiler_dir% /EHsc /Zi^
   /DEBUG:FULL^
   /INCREMENTAL:NO^
   /Fe:"life-in-urbantopia"^
-  ../src/main.cpp
+  ../src/main.cpp^
+  /I%sdl_include_dir%^
+  /link /LIBPATH:%sdl_link_dir% SDL2.lib
 
 popd
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,66 @@
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
 #include <iostream>
 #include <string>
+
 
 // Engine state - initial message
 const std::string engineState = "Tech Demo"; // No version because of no plan
 
-int main() {
+int main(void) {
     std::cout << "Life In Ubrantopia - " << engineState << std::endl;
+
+    // SDL initialization
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    // Create window
+    SDL_Window* window = SDL_CreateWindow("An SDL2 window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_OPENGL);
+    if (window == NULL) {
+        std::cout << "Could not create window: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    // Create renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        std::cout << "Could not create renderer: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    SDL_Event event;
+    bool running = true;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            // Global events
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+
+            // Keyboard events
+            if (event.type == SDL_KEYDOWN) {
+                switch (event.key.keysym.sym) {
+                    case SDLK_ESCAPE:
+                        running = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+        SDL_RenderClear(renderer); // Clear the screen (to red)
+        SDL_RenderPresent(renderer); // Draw to the screen
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    std::cout << "All went well, goodbye!" << std::endl;
     return 0;
 }
